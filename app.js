d4945a0784f1be53a1924ec6b3f718126e316f85
jasper-2020 <jasper.web@gmail.com>
2023-02-07 11:28:55 +0700

    ok

--- app.js
+++ app.js
@@ -3,15 +3,53 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+///// start socket
+const server = require('http').createServer(app);
+const port = process.env.PORTx || 8080;
+// const io = require('socket.io')(server);
+const io = require('socket.io')(server,{
+    cors: {origin: ["http://localhost:8080", "http://localhost:3000", "https://glitch.me", "https://cdpn.io"]}
+  });
+app.io = io;
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.emit('tx', 'msg');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+server.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+}); 
+///// end socket
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+/////////////////// cors
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://glitch.me",
+  "https://cdpn.io",
+];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: whitelist,
+};
+
+app.use(cors(corsOptions));
+///////////////////
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index_socket', { title: 'Express Socket' });
+});
+
+router.get('/test',  function(req, res) {
+  req.app.io.emit('tx', {key:"value22"});
   res.render('index', { title: 'Express' });
 });
 
